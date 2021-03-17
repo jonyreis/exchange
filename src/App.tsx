b8493098dcddcb579271e259324981a77e357d41
Jony Reis <jonyreiscardoso@gmail.com>
2021-03-17 00:05:40 -0400

    remove redux/toolkit and add redux

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,10 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
-import { Provider } from 'react-redux';
-
-import store from './store';
 import Routes from './routes';
 
 const App: React.FC = () => (
   <BrowserRouter>
-    <Provider store={store}>
-      <Routes />
-    </Provider>
+    <Routes />
   </BrowserRouter>
 );
 
