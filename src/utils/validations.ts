b2f61493e8714ac2dadfdd25e667f3152d1ee340
Jony Reis <jonyreiscardoso@gmail.com>
2021-03-17 23:42:30 -0400

    translate validation description to Portuguese

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -1,10 +1,10 @@
 
-export const required = (value: string) => (value ? null : 'Required.');
+export const required = (value: string) => (value ? null : 'Campo obrigatório.');
 
-export const minValue = (min: number) => (value: string) => min > value.length && `Should be  at least ${min} digits.`
+export const minValue = (min: number) => (value: string) => min > value.length && `Deve ter pelo menos ${min} digitos.`
 
 export const isEmail = (value: string) =>
-  !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ? 'Invalid email address' : null;
+  !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(value) ? 'Email inválido' : null;
 
 export const composeValidators = (...validators: any[]) => (value: any) =>
   validators.reduce((error, validator) => error || validator(value), null);
