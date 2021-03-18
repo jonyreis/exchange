b4a0b52a77c5c0bbd84ddafad268ace18057b7e8
Jony Reis <jonyreiscardoso@gmail.com>
2021-03-17 21:05:54 -0400

    add font Object Sans

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,6 +1,12 @@
 import { createGlobalStyle } from 'styled-components';
 
+import ObjectSans from './ObjectSans-Regular.otf';
+
 export default createGlobalStyle`
+  @font-face {
+    font-family: Object Sans;
+    src: url(${ObjectSans});
+  }
   html, body, div, span, iframe, h1, h2, h3, h4, h5, h6, p, blockquote, a, em, img, dl, dt, dd, ol, ul, li, fieldset, form, label, input, legend, article, footer, header, nav, section, main {
     margin: 0;
     padding: 0;
@@ -11,6 +17,10 @@ export default createGlobalStyle`
     min-height: 100%;
   }
 
+  body {
+    font-family: Object Sans, sans-serif;
+    -webkit-font-smoothing: antialiased !important;
+  }
 
   ul, ol {
     list-style: none;
