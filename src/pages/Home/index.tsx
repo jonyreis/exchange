c0771568928cd78bc9b1a9c44be5781310c4b520
Jony Reis <jonyreiscardoso@gmail.com>
2021-03-18 00:49:52 -0400

    add useSelector on Home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,25 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
+
+import appFire from '../../firebase'
+import 'firebase/auth'
+
+import { IUser } from '../../store/modules/userAuth/types'
+import { IState } from '../../store'
 
 const Home: React.FC = () => {
+  const userAuthenticated = useSelector<IState, IUser>((state) => state.userAuth.user);
+
+  React.useEffect(() => {
+    console.log(userAuthenticated)
+    appFire.auth().onAuthStateChanged(user => {
+      if (user) {
+        console.log(user.email)
+      } else {
+      }
+    })
+  }, [])
+
   return (
     <div>
       <h1>Home</h1>
