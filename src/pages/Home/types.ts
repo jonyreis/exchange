047ff6768d02fa7d321fed3d3af55eb941c1fbee
Jony Reis <jonyreiscardoso@gmail.com>
2021-03-18 13:32:45 -0400

    add types interface IBitcoin and IBritas

---
+++ src/pages/Home/types.ts
@@ -0,0 +1,16 @@
+export interface IBitcoin {
+  buy: string,
+  date: number,
+  high: string,
+  last: string,
+  low: string,
+  open: string,
+  sell: string,
+  vol: string,
+}
+
+export interface IBritas {
+  cotacaoCompra: number,
+  cotacaoVenda: number,
+  dataHoraCotacao: string,
+}
