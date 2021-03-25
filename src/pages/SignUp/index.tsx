fb48d2ad498bbac1894c65b997cf3a4ae1ac77c6
Jony Reis <jonyreiscardoso@gmail.com>
2021-03-25 07:30:50 -0400

    fix prettier formater on SignUp

--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -3,47 +3,54 @@ import { Link, useHistory } from 'react-router-dom'
 import { Form, Field } from 'react-final-form'
 import { FORM_ERROR } from 'final-form'
 
-import { composeValidators, required, minValue, isEmail } from '../../utils/validations'
+import {
+  composeValidators,
+  required,
+  minValue,
+  isEmail
+} from '../../utils/validations'
 
 import appFire from '../../firebase'
 import 'firebase/auth'
 
 import { SignUpContainer } from '../styles'
+import { IUser } from '../../store/modules/userAuth/types'
 
 const SignUp: React.FC = () => {
+  const history = useHistory()
 
-  const history = useHistory();
-
-  const onSubmit = async (values: { email: string; password: string; confirm: string }) => {
+  async function onSubmit(values: IUser) {
     if (values.password === values.confirm) {
       appFire
         .auth()
         .createUserWithEmailAndPassword(values.email, values.password)
         .then(() => {
-          alert('Conta criada com sucesso!!');
+          alert('Conta criada com sucesso!!')
           history.push('/')
         })
-        .catch((error) => {
+        .catch(error => {
           switch (error.code) {
-            case "auth/email-already-in-use":
-              alert('Há uma conta cadastrada com este email!!');
+            case 'auth/email-already-in-use':
+              alert('Há uma conta cadastrada com este email!!')
               break
-            case "auth/invalid-email":
-              alert('Email inválido!!');
-              break;
-            case "auth/weak-password":
-              alert('Senha fraca!!');
-              break;
+            case 'auth/invalid-email':
+              alert('Email inválido!!')
+              break
+            case 'auth/weak-password':
+              alert('Senha fraca!!')
+              break
             default:
-              alert('Erro não identificado!!');
+              alert('Erro não identificado!!')
               console.log('Erro não identificado')
-              break;
+              break
           }
         })
     }
     if (values.password !== values.confirm) {
       return { [FORM_ERROR]: 'Confirmação de senha errada!' }
     }
+
+    return false
   }
 
   return (
@@ -53,14 +60,16 @@ const SignUp: React.FC = () => {
         onSubmit={onSubmit}
         render={({ handleSubmit, submitting, submitError }) => (
           <form onSubmit={handleSubmit}>
-            <Field
-              name="email"
-              validate={composeValidators(required, isEmail)}
-            >
+            <Field name="email" validate={composeValidators(required, isEmail)}>
               {({ input, meta }) => (
                 <div className="field">
-                  <label>Email</label>
-                  <input {...input} type="email" placeholder="Digite seu email" />
+                  <label htmlFor="email">Email</label>
+                  <input
+                    {...input}
+                    name="email"
+                    type="email"
+                    placeholder="Digite seu email"
+                  />
                   {meta.error && meta.touched && <span>{meta.error}</span>}
                 </div>
               )}
@@ -71,8 +80,13 @@ const SignUp: React.FC = () => {
             >
               {({ input, meta }) => (
                 <div className="field">
-                  <label>Senha</label>
-                  <input {...input} type="password" placeholder="Digite sua senha" />
+                  <label htmlFor="senha">Senha</label>
+                  <input
+                    {...input}
+                    name="senha"
+                    type="password"
+                    placeholder="Digite sua senha"
+                  />
                   {meta.error && meta.touched && <span>{meta.error}</span>}
                 </div>
               )}
@@ -83,8 +97,13 @@ const SignUp: React.FC = () => {
             >
               {({ input, meta }) => (
                 <div className="field">
-                  <label>Cofirmar Senha</label>
-                  <input {...input} type="password" placeholder="Confirme sua senha" />
+                  <label htmlFor="confirmar">Cofirmar Senha</label>
+                  <input
+                    {...input}
+                    name="confirmar"
+                    type="password"
+                    placeholder="Confirme sua senha"
+                  />
                   {meta.error && meta.touched && <span>{meta.error}</span>}
                 </div>
               )}
@@ -96,7 +115,8 @@ const SignUp: React.FC = () => {
               </button>
             </div>
             <div className="voltar">
-              <p>Já possui cadastro?</p><Link to="/">Entrar</Link>
+              <p>Já possui cadastro?</p>
+              <Link to="/">Entrar</Link>
             </div>
           </form>
         )}
